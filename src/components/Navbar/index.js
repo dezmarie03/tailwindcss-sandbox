64c1a87c01ea470a444e899b85c7c457d368c623
Dez Beberniss <dezmarie@gmail.com>
2021-11-30 15:22:52 -0800

    Fix compilation errors

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,12 +18,12 @@ const Navbar = ({ items = [] }) => {
   const menuKeyHandler = event => {
     // Define keys to listen for
     let actions = {
-      arrowDown: event.key == "ArrowDown",
-      enter: event.key == "Enter",
-      escape: event.key == "Escape",
+      arrowDown: event.key === "ArrowDown",
+      enter: event.key === "Enter",
+      escape: event.key === "Escape",
       shift: event.shiftKey,
-      space: event.key == " ",
-      tab: event.key == "Tab",
+      space: event.key === " ",
+      tab: event.key === "Tab",
     }
 
     // Anywhere: Close the menu if Escape key is pressed
@@ -49,9 +49,9 @@ const Navbar = ({ items = [] }) => {
       menuWrapperRef.current.querySelector('a:first-of-type').focus();
       document.addEventListener('click', handleOutsideClick);
     } else {
-      document.removeEventListener("click", handleOutsideClick);
+      document.removeEventListener('click', handleOutsideClick);
     }
-  }, [showMenu]);
+  });
 
   const Logo = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-pink-600"><path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 12c-1.657 0-3 .895-3 2s1.343 2 3 2 3-.895 3-2V3z" />
   </svg>;
